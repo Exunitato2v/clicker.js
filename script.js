55da04a49acfbbfd945b5713f275da843fad8cae
Exunitato2v <145047880+Exunitato2v@users.noreply.github.com>
2023-11-30 19:51:34 +0300

    +Sound

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let circle = document.querySelector(".circle")
+
+let interaction = true;
+
 circle.addEventListener("click", function() {
+
     clickedanim()
     UpdateValue(1)
+    randomSound()
 })
 
 let counter = document.querySelector(".counter")
@@ -12,6 +17,14 @@ function clickedanim() {
         circle.classList.remove("clicked")
     }, 100);
 
+
+}
+
+function randomSound() {
+    let random = Math.random();
+    let index = Math.floor(random * 10) % 5
+    console.log(AudioArray[index])
+    AudioArray[index].play();
 }
 
 let clickvalue = 1
@@ -19,3 +32,24 @@ let clickvalue = 1
 function UpdateValue(value) {
     counter.innerHTML = +counter.innerHTML + value
 }
+let AudioArray = [];
+
+let comSound = new Audio('./audio/com.mp3')
+let dollarSound = new Audio('./audio/dollar.mp3')
+let popSound = new Audio('./audio/pop.mp3')
+let ppoSound = new Audio('./audio/ppo.mp3')
+let travisSound = new Audio('./audio/travis.mp3')
+
+travisSound.autoplay = false;
+comSound.autoplay = false;
+dollarSound.autoplay = false;
+popSound.autoplay = false;
+ppoSound.autoplay = false;
+
+AudioArray.push(comSound)
+AudioArray.push(dollarSound)
+AudioArray.push(popSound)
+AudioArray.push(ppoSound)
+AudioArray.push(travisSound)
+
+console.log(AudioArray)
